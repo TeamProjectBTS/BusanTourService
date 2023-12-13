5eebbe2ffd27ce0dc74e82e74d9c80b8f3520d4b
UmJoonSic <csm893516@gmail.com>
2023-12-13 09:46:44 +0900

    no message

--- src/main/java/com/example/board/SpringBootBoardV1Application.java
+++ src/main/java/com/example/board/SpringBootBoardV1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpiringBootBoardV1Application {
+public class SpringBootBoardV1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpiringBootBoardV1Application.class, args);
+		SpringApplication.run(SpringBootBoardV1Application.class, args);
 	}
 
 }
--- src/test/java/com/example/board/SpringBootBoardV1ApplicationTests.java
+++ src/test/java/com/example/board/SpringBootBoardV1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpiringBootBoardV1ApplicationTests {
+class SpringBootBoardV1ApplicationTests {
 
 	@Test
 	void contextLoads() {
