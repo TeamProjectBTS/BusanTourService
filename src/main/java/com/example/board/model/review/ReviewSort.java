e0a98f050ab3c362b5bbc3236ce41cd75b79f019
UmJoonSic <csm893516@gmail.com>
2023-12-16 11:56:05 +0900

    리뷰 기능구현 준비

---
+++ src/main/java/com/example/board/model/review/ReviewSort.java
@@ -0,0 +1,11 @@
+package com.example.board.model.review;
+
+import lombok.Getter;
+
+@Getter
+public enum ReviewSort {
+	TOUR_SPOT,
+	RESTAURANT,
+	HOTEL;
+	
+}
--- src/main/java/com/example/board/model/review/ReviewWriteForm.java
+++ src/main/java/com/example/board/model/review/ReviewWriteForm.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import lombok.Data;
 
 @Data
-public class Review {
+public class ReviewWriteForm {
 	
 	private Long review_id;
 	private String member_id;
